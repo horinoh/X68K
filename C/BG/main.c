74df50b62729fc50536b080f145f3d39bcaa756e
horinoh <development@horino.sakura.ne.jp>
2022-12-26 16:54:01 +0900

    BG

--- C/BG/main.c
+++ C/BG/main.c
@@ -3,123 +3,141 @@
 #include <iocslib.h>
 
 #include "../Common.h"
-#include "../PCG.h"
 #include "../Palette.h"
+#include "../PCG.h"
+//#include "../Sprite.h"
 #include "../BG.h"
 #include "../Joy.h"
 
-#define ESC_ON (BITSNS(0) & (1 << 1))
+//0, 0, 0, 1, 1, 0, 0, 0,
+//0, 0, 1, 1, 1, 1, 0, 0,
+//0, 1, 1, 1, 1, 1, 1, 0,
+//1, 1, 0, 1, 1, 0, 1, 1,
+//1, 1, 1, 1, 1, 1, 1, 1,
+//0, 0, 1, 0, 0, 1, 0, 0,
+//0, 1, 0, 1, 1, 0, 1, 0,
+//1, 0, 1, 0, 0, 1, 0, 1,
 
-uint64_t PALData[PAL_BLOCK_COUNT][PAL_COLOR_COUNT];
-uint8_t	PCGData[PCG_COUNT][PCG_16X16_SIZE];
+//0, 0, 0, 0, 1, 1, 0, 0,
+//0, 0, 0, 0, 1, 0, 0, 0,
+//0, 1, 1, 1, 1, 1, 1, 0,
+//1, 1, 1, 1, 1, 1, 1, 1,
+//1, 0, 1, 1, 1, 1, 1, 1,
+//1, 0, 1, 1, 1, 1, 1, 1,
+//1, 1, 1, 1, 1, 1, 1, 1,
+//0, 1, 1, 1, 1, 1, 1, 0,
+uint8_t PCGData8X8[] = {
+  PIX2(0, 0), PIX2(0, 1), PIX2(1, 0), PIX2(0, 0),
+  PIX2(0, 0), PIX2(1, 1), PIX2(1, 1), PIX2(0, 0),
+  PIX2(0, 1), PIX2(1, 1), PIX2(1, 1), PIX2(1, 0),
+  PIX2(1, 1), PIX2(0, 1), PIX2(1, 0), PIX2(1, 1),
+  PIX2(1, 1), PIX2(1, 1), PIX2(1, 1), PIX2(1, 1),
+  PIX2(0, 0), PIX2(1, 0), PIX2(0, 1), PIX2(0, 0),
+  PIX2(0, 1), PIX2(0, 1), PIX2(1, 0), PIX2(1, 0),
+  PIX2(1, 0), PIX2(1, 0), PIX2(0, 1), PIX2(0, 1),
 
-enum {
-  SP_PRI_INVISIBLE,
-  //!< Back of BG1
-  SP_PRI_BACK,
-  //!< Between BG0 and BG1
-  SP_PRI_MIDDLE,
-  //!< Front of BG0
-  SP_PRI_FRONT,
+  PIX2(0, 0), PIX2(0, 0), PIX2(1, 1), PIX2(0, 0),
+  PIX2(0, 0), PIX2(0, 0), PIX2(1, 0), PIX2(0, 0),
+  PIX2(0, 1), PIX2(1, 1), PIX2(1, 1), PIX2(1, 0),
+  PIX2(1, 1), PIX2(1, 1), PIX2(1, 1), PIX2(1, 1),
+  PIX2(1, 0), PIX2(1, 1), PIX2(1, 1), PIX2(1, 1),
+  PIX2(1, 0), PIX2(1, 1), PIX2(1, 1), PIX2(1, 1),
+  PIX2(1, 1), PIX2(1, 1), PIX2(1, 1), PIX2(1, 1),
+  PIX2(0, 1), PIX2(1, 1), PIX2(1, 1), PIX2(1, 0),
 };
 
-#define NO_VSYNC 0
-#define WITH_VSYNC (1 << 31)
+uint64_t PALData[PAL_BLOCK_COUNT][PAL_COLOR_COUNT];
+uint8_t	PCGData[PCG_16X16_COUNT][PCG_16X16_SIZE];
 
 void main()
 {
   //!< CRT mode
-  CRTMOD(6);
+  const int PrevCRT = CRTMOD(-1);
+  CRTMOD(MODE_512_256X256_C16_P4_31KHZ);
   //!< Cursor off
   B_CUROFF();
   //!< Clear graphics
   G_CLR_ON(); 
- 
-  //!< Load palette data
-  {
-    FILE* Fp = fopen("../../x68k_xsp/SAMPLE/PANEL.PAL", "rb");
-      if(NULL != Fp) {
-      puts("Loading palette...");
-      fread(PALData, sizeof(PALData[0][0]), COUNTOF(PALData), Fp);
-      fclose(Fp);
-    }
-  }
-  //!< Load PCG data
-  {
-    FILE* Fp = fopen("../../x68k_xsp/SAMPLE/PANEL.SP", "rb");
-    if(NULL != Fp) {
-      puts("Loading pattern...");
-      fread(PCGData, sizeof(PCGData[0][0]), COUNTOF(PCGData), Fp);
-      fclose(Fp);
-    }
-  }
+  SP_INIT();
+  
+  //!< Palette
+  SPALET(0, 1, COL_BLACK);
+  SPALET(1, 1, COL_RED);
 
-  int SpX = 128, SpY = 128;
-  {
-    SP_INIT();
-    SP_ON();
+  //!< PCG
+  SP_CGCLR(0);
+  SP_DEFCG(128, PCG_PAT_8X8, &PCGData8X8[0]);
+  SP_DEFCG(129, PCG_PAT_8X8, &PCGData8X8[32]);
 
-    SP_CGCLR(0);
-    for (int i = 0; i < COUNTOF(PCGData); ++i) {
-      SP_DEFCG(i, PCG_PAT_16X16, PCGData[i]);
-    }
+  //!< Sprite (Need SP_ON(), even if BG only)
+  SP_ON(); 
 
-    for (int i = 0; i < COUNTOF(PALData); ++i) {
-      for (int j = 0; j < COUNTOF(PALData[0]); ++j) {
-	  	  SPALET(WITH_VSYNC | j, i, PALData[i][j]);
-      }
-	  }
-
-    SP_REGST(WITH_VSYNC | 0, SpX, SpY, CODE(FLIP_NONE, 0, 0), SP_PRI_FRONT);
-  }
-
+  //!< BG
+  int BG0PCG = 128, BG1PCG = 129;
+  int BG0Pal = 1, BG1Pal = 1;
   int Bg0X = 0, Bg0Y = 0;
   int Bg1X = 0, Bg1Y = 0;
   {
-    //!< BG0 PAGE0 ON
+    //!< BG0 PAGE0 ONOFF
     BGCTRLST(0, 0, BG_ON);
-    //!< BG1 PAGE1 ON
-	  BGCTRLST(1, 1, BG_ON);
+    //!< BG1 PAGE1 ONOFF
+	  BGCTRLST(1, 1, BG_OFF);
 
+    //!< Clear map
     BGTEXTCL(0, 0); 
     BGTEXTCL(1, 0);
+    //!< Set map
     for (int i = 0; i < BG_HEIGHT; ++i) {
       for (int j = 0; j < BG_WIDTH; ++j) {
-        BGTEXTST(0, j, i, CODE(FLIP_NONE, 0, 0)); 
-        BGTEXTST(1, j, i, CODE(FLIP_NONE, 0, 1)); 
+        BGTEXTST(0, j, i, CODE(FLIP_NONE, BG0Pal, BG0PCG)); 
+        BGTEXTST(1, j, i, CODE(FLIP_NONE, BG1Pal, BG1PCG)); 
       }
     }
 
     //!< BG0 SCROLL
-    BGSCRLST(WITH_VSYNC | 0, Bg0X, Bg0Y);
+    BGSCRLST(0, Bg0X, Bg0Y);
     //!M BG1 SCROLL 
-    BGSCRLST(WITH_VSYNC | 1, Bg1X, Bg1Y);
+    BGSCRLST(1, Bg1X, Bg1Y);
   }
-  
+
   while (1)
   {      
     if(ESC_ON) { break; }
       
     const int Joy = JOYGET(0);
-    if(!(Joy & JOY_UP)) { --SpY; }
-    if(!(Joy & JOY_DOWN)) { ++SpY; }
-    if(!(Joy & JOY_LEFT)) { --SpX; }
-    if(!(Joy & JOY_RIGHT)) { ++SpX; }
-    if(!(Joy & JOY_A)) { break; }
-    if(!(Joy & JOY_B)) { break; }
-    SpX = MAX(MIN(SpX, 255), 0);
-    SpY = MAX(MIN(SpY, 255), 0);
-    SP_REGST(WITH_VSYNC | 0, SpX, SpY, CODE(FLIP_NONE, 0, 0), SP_PRI_FRONT);
+    if(!(Joy & JOY_UP)) { }
+    if(!(Joy & JOY_DOWN)) { }
+    if(!(Joy & JOY_LEFT)) { }
+    if(!(Joy & JOY_RIGHT)) { }
+    if(UP_ON) { }
+    if(DOWN_ON) { }
+    if(LEFT_ON) { }
+    if(RIGHT_ON) { }
 
-    Bg0X = MIN(++Bg0X, 1024);
-    Bg1Y = MIN(++Bg1Y, 1024);
-    BGSCRLST(WITH_VSYNC | 0, Bg0X, Bg0Y);
-    BGSCRLST(WITH_VSYNC | 1, Bg1X, Bg1Y);
+    Bg0X = ++Bg0X & 0x3ff;
+    Bg1Y = ++Bg1Y & 0x3ff;
+    BGSCRLST(ON_VSYNC | 0, Bg0X, Bg0Y);
+    BGSCRLST(ON_VSYNC | 1, Bg1X, Bg1Y);
 
-    B_LOCATE(0, 2);
-    printf("%03d, %03d\n", SpX, SpY);
-    printf("%03d, %03d\n", Bg0X, Bg0Y);
-    printf("%03d, %03d\n", Bg1X, Bg1Y);
+    if(TAB_ON) {
+      if(BGCTRLGT(0)){
+        BGCTRLST(0, 0, BG_OFF);
+	      BGCTRLST(1, 1, BG_ON);
+      } else {
+        BGCTRLST(0, 0, BG_ON);
+	      BGCTRLST(1, 1, BG_OFF);
+      }
+    }
+    if(SPACE_ON) {
+      BGCTRLST(0, 0, BG_ON);
+	    BGCTRLST(1, 1, BG_ON);
+    }
+
+    //SPALET(ON_VSYNC | 0, 0, XXX);
+
+    B_LOCATE(0, 0);
+    printf("BG0 %03d, %03d\n", Bg0X, Bg0Y);
+    printf("BG1 %03d, %03d\n", Bg1X, Bg1Y);
   }
   
   SP_OFF();
@@ -127,5 +145,5 @@ void main()
   //!< Cursor on
   B_CURON();
   //!< CRT mode
-  CRTMOD(0x10);
+  CRTMOD(PrevCRT);
 }
